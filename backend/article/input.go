edaead93ee29a87f258635a0df1a365d941f5c21
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-07-11 23:48:17 +0700

    update: input object name

--- backend/article/input.go
+++ backend/article/input.go
@@ -1,6 +1,6 @@
 package article
 
-type InputNewArticel struct {
+type InputNewArticle struct {
 	Title    string `json:"title" binding:"required,min=20"`
 	Content  string `json:"content" binding:"required,min=200"`
 	Category string `json:"category" binding:"required,min=3"`
