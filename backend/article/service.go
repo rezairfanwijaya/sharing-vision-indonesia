63c036815b6d3917c8b2013a7c0df7222d030135
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-07-11 23:49:00 +0700

    add: service to save new article

--- backend/article/service.go
+++ backend/article/service.go
@@ -1,10 +1,18 @@
 package article
 
+import (
+	"fmt"
+	"net/http"
+	"svid/helper"
+	"time"
+)
+
 type IService interface {
-	Save()
-	GetByID()
-	Update()
-	Delete()
+	Save(input InputNewArticle) (Article, int, error)
+	// GetByID(ID int) (Article, error)
+	// GetAll() ([]Article, error)
+	// Update(ID int) error
+	// Delete(ID int) error
 }
 
 type service struct {
@@ -13,4 +21,40 @@ type service struct {
 
 func NewService(repoArticle IRepository) *service {
 	return &service{repoArticle}
+}
+
+func (s *service) Save(input InputNewArticle) (Article, int, error) {
+	// cari apakah ada title yang sama
+	articleByTitle, err := s.repoArticle.GetByTitle(input.Title)
+	if err != nil {
+		return articleByTitle, http.StatusInternalServerError, err
+	}
+
+	// tidak boleh ada judul yang sama
+	if articleByTitle.Title == input.Title {
+		return articleByTitle, http.StatusConflict, fmt.Errorf("judul %v sudah dipakai", input.Title)
+	}
+
+	// validasi status
+	isStatusValid := helper.IsStatusValid(input.Status)
+	if !isStatusValid {
+		return articleByTitle, http.StatusBadRequest, fmt.Errorf("status yang didukung hanya publish, draft atau thrash")
+	}
+
+	// binding
+	article := Article{
+		Title:       input.Title,
+		Content:     input.Content,
+		Category:    input.Category,
+		CreatedDate: time.Now(),
+		Status:      input.Status,
+	}
+
+	// save
+	newArticle, err := s.repoArticle.Save(article)
+	if err != nil {
+		return newArticle, http.StatusInternalServerError, err
+	}
+
+	return newArticle, http.StatusOK, nil
 }
