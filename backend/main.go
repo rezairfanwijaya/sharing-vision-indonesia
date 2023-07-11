347c977855e5b45b61c9b6314c796b119a2725b5
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-07-11 22:01:42 +0700

    add: connection object

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"log"
+	"svid/database"
+)
 
+func main() {
+	// koneksi ke database
+	dbConnection, err := database.NewConnection("./.env")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	log.Println(dbConnection)
 }
