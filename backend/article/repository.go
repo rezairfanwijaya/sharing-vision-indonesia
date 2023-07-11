d190cf5d318173c245709e508f825df9e17feac0
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-07-11 23:48:35 +0700

    add: repository implementation

--- backend/article/repository.go
+++ backend/article/repository.go
@@ -3,10 +3,12 @@ package article
 import "gorm.io/gorm"
 
 type IRepository interface {
-	Save()
-	GetByID()
-	Update()
-	Delete()
+	Save(article Article) (Article, error)
+	GetByID(ID int) (Article, error)
+	GetByTitle(title string) (Article, error)
+	GetAll() ([]Article, error)
+	Update(article Article) error
+	Delete(ID int) error
 }
 
 type repository struct {
@@ -15,4 +17,56 @@ type repository struct {
 
 func NewRepository(db *gorm.DB) *repository {
 	return &repository{db}
+}
+
+func (r *repository) Save(article Article) (Article, error) {
+	if err := r.db.Create(&article).Error; err != nil {
+		return article, err
+	}
+
+	return article, nil
+}
+
+func (r *repository) GetByID(ID int) (Article, error) {
+	var article Article
+	if err := r.db.Where("id = ?", ID).Find(&article).Error; err != nil {
+		return article, err
+	}
+
+	return article, nil
+}
+
+func (r *repository) GetByTitle(title string) (Article, error) {
+	var article Article
+	if err := r.db.Where("title = ?", title).Find(&article).Error; err != nil {
+		return article, err
+	}
+
+	return article, nil
+}
+
+func (r *repository) GetAll() ([]Article, error) {
+	var articles []Article
+	if err := r.db.Find(&articles).Error; err != nil {
+		return articles, err
+	}
+
+	return articles, nil
+}
+
+func (r *repository) Update(article Article) error {
+	if err := r.db.Save(&article).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *repository) Delete(ID int) error {
+	var article Article
+	if err := r.db.Where("id = ?", ID).Delete(&article).Error; err != nil {
+		return err
+	}
+
+	return nil
 }
