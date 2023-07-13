790fef49d41bc2b9f293d1b162fae66048fd5738
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-07-13 17:54:14 +0700

    add: setup

---
+++ frontend/src/constant/index.js
@@ -0,0 +1,6 @@
+const DOMAIN = "http://localhost:8181/"
+const FAILED = 400
+const CONFLICT = 409
+const SUCCESS = 200
+const BAD_REQUEST = 404
+export { DOMAIN, FAILED, SUCCESS, CONFLICT, BAD_REQUEST }
